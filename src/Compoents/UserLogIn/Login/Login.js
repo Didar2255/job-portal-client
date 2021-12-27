2497d44f84ab18498724eaee6f71884cf90aa0ac
Didar2255 <didar.a2255@gmail.com>
2021-12-27 18:14:36 +0600

    style login and registration from

--- src/Compoents/UserLogIn/Login/Login.js
+++ src/Compoents/UserLogIn/Login/Login.js
@@ -1,30 +1,43 @@
-import { Button, Container, Grid, TextField } from '@mui/material';
+import { Button, Container, Grid, TextField, Typography } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
+    const submitFrom = (e) => {
+        e.preventDefault()
+    }
     return (
         <Container sx={{ m: 8 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
-                    <form action="" className='login-form'>
+                    <form onSubmit={submitFrom} className='login-form'>
+                        <Link to='/login'>
+                            <Button variant="contained" sx={{ background: '#182f59', width: '50%', borderRadius: 0 }} type='submit'>SIGN IN</Button>
+                        </Link>
+                        <Link to='/registration'>
+                            <Button variant="contained" sx={{ background: '#182f59', width: '50%', borderRadius: 0 }} type='submit'>SIGN UP</Button>
+                        </Link>
+                        <Typography variant='h6'>
+                            SIGN IN
+                        </Typography>
                         <TextField
                             sx={{ m: 1, width: '75%' }}
                             id="outlined-basic"
-                            type='Text'
-                            name='name'
-                            onBlur=''
-                            placeholder='User Name'
-                            variant="outlined" />
-                        <TextField
-                            sx={{ m: 1, width: '75%' }}
-                            id="outlined-basic"
+                            label="User Email"
                             type='email'
                             name='email'
                             onBlur=''
-                            placeholder='User Email'
                             variant="outlined" />
-                        <Button variant="contained" sx={{ backgroundColor: '', borderRadius: 1, m: 1, width: '20%' }} type='submit'>SIGN UP</Button><br />
+                        <TextField
+                            sx={{ m: 1, width: '75%' }}
+                            id="outlined-basic"
+                            label='User Password'
+                            type='password'
+                            name='password'
+                            onBlur=''
+                            variant="outlined" />
+                        <Button variant="contained" sx={{ background: '#182f59', borderRadius: 1, m: 1, width: '20%' }} type='submit'>SIGN UP</Button>
                     </form>
                 </Grid>
                 <Grid item xs={12} md={6}>
--- src/Compoents/UserLogIn/Registration/Registration.js
+++ src/Compoents/UserLogIn/Registration/Registration.js
@@ -1,5 +1,7 @@
 import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Registration.css'
 
 const Registration = () => {
     return (
@@ -7,46 +9,86 @@ const Registration = () => {
             <Box sx={{ flexGrow: 1 }}>
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={6}>
-                        <Typography variant='h6' sx={{ lineHeight: 6, fontWeight: 500 }}>
-                            SIGN UP
-                        </Typography>
-                        <Typography variant='body2' sx={{ lineHeight: 6, fontWeight: 500 }}>
-                            Register To Get A Job
-                        </Typography>
-                        <form onClick=''>
+                        <form onClick='' className='register-form'>
+                            <Box sx={{ mb: 5 }}>
+                                <Link to='/login'>
+                                    <Button variant="contained" sx={{ background: '#182f59', width: '50%', borderRadius: 0 }} type='submit'>SIGN IN</Button>
+                                </Link>
+                                <Link to='/registration'>
+                                    <Button variant="contained" sx={{ background: '#182f59', width: '50%', borderRadius: 0 }} type='submit'>SIGN UP</Button>
+                                </Link>
+                            </Box>
+                            <Typography variant='h6' sx={{ fontWeight: 700 }}>
+                                SIGN UP
+                            </Typography>
+                            <Typography variant='body2' sx={{ fontWeight: 500, mb: 5 }}>
+                                Register To Get A Job
+                            </Typography>
+
                             <TextField
-                                sx={{ m: 1, width: '75%' }}
-                                id="standard-basic"
-                                type='Text'
+                                id="outlined-basic"
+                                sx={{ m: 1 }}
+                                label="First Name"
+                                type='text'
                                 name='name'
                                 onBlur=''
-                                placeholder='User Name'
-                                variant="standard" />
+                                variant="outlined"
+                            />
                             <TextField
+                                id="outlined-basic"
+                                sx={{ m: 1 }}
+                                label="Phone Number"
+                                type='number'
+                                name='number'
+                                onBlur=''
+                                variant="outlined"
+                            />
+                            <TextField
+                                id="outlined-basic"
+                                sx={{ m: 1 }}
+                                type='date'
+                                name='date'
+                                onBlur=''
+                                variant="outlined"
+                            />
+                            <TextField
+                                id="outlined-basic"
+                                sx={{ m: 1 }}
+                                label="Gender"
+                                type='text'
+                                name='gender'
+                                onBlur=''
+                                variant="outlined"
+                            />
+                            <TextField
+                                id="outlined-basic"
                                 sx={{ m: 1, width: '75%' }}
-                                id="standard-basic"
+                                label="Email"
                                 type='email'
                                 name='email'
                                 onBlur=''
-                                placeholder='User Email'
-                                variant="standard" />
+                                variant="outlined"
+                            />
                             <TextField
-                                sx={{ m: 1, width: '75%' }}
-                                id="standard-basic"
+                                id="outlined-basic"
+                                sx={{ m: 1 }}
+                                label="Password"
                                 type='password'
                                 name='password'
                                 onBlur=''
-                                placeholder='User Password'
-                                variant="standard" />
+                                variant="outlined"
+                            />
                             <TextField
-                                sx={{ m: 1, width: '75%' }}
-                                id="standard-basic"
+                                id="outlined-basic"
+                                sx={{ m: 1 }}
+                                label="Confirm Password"
                                 type='password'
-                                name='password2'
+                                name='password'
                                 onBlur=''
-                                placeholder='Re-type password'
-                                variant="standard" />
-                            <Button variant="contained" sx={{ backgroundColor: '#2CD4ED', borderRadius: 0, m: 1, width: '75%' }} type='submit'>SIGN UP</Button><br />
+                                variant="outlined"
+                            />
+
+                            <Button variant="contained" sx={{ backgroundColor: '#2CD4ED', borderRadius: 1, m: 1, width: '20%' }} type='submit'>SIGN UP</Button><br />
                         </form>
 
                     </Grid>
